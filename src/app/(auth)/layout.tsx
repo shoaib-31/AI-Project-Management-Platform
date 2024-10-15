3fb7d435ae09e754cb328ea41bdba512b59b4947
Shoaib Akhtar <shoaibakmasood@gmail.com>
2024-10-16 03:07:58 +0530

    feat: initial project setup

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayoutProps = ({ children }: AuthLayoutProps) => {
+  return (
+    <main className=" bg-neutral-100 min-h-screen">
+      <div className=" mx-auto max-w-screen-2xl p-4">
+        <nav className=" flex justify-between items-center">
+          <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+          <Button variant={"secondary"}>Sign Up</Button>
+        </nav>
+        <div className=" flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayoutProps;
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export const SignInCard = () => {
+  return (
+    <Card className=" w-full h-full md:w-[486px] border-none shadow-none">
+      <CardHeader>
+        <CardTitle>Welcome back!</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
